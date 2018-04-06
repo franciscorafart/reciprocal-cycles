6414d5b10477b2fb53bba07c0d15374ca0fcd94b
Francisco Rafart <francisco.rafart@gmail.com>
2018-04-06 19:36:47 -0400

    index of first character

--- euler26.js
+++ euler26.js
@@ -2,8 +2,9 @@
 let i = 0
 console.log(div(1,3))
 
-function div(x,y,str){
+function div(x,y,str, count){
   let res = str || '';
+  let i = count || 0
 
   let reminder = 0;
   let division = x/y;
@@ -11,7 +12,7 @@ function div(x,y,str){
   reminder = x%y;
   let newY = y
 
-  if (i > 3){
+  if (i > 50){
     return res;
   }
 
@@ -22,7 +23,7 @@ function div(x,y,str){
     } else {
         res += String(whole)+'.';
         i+=1
-        return div(reminder*10,newY,res)
+        return div(reminder*10,newY,res, i)
     }
   }
 
@@ -33,11 +34,34 @@ function div(x,y,str){
     } else {
         res += String(whole)
         i+= 1;
-        return div(reminder*10,newY,res)
+        return div(reminder*10,newY,res, i)
     }
   }
 }
 
+const findRecurrent = str => {
+  //find indexes of first item
+  let firstChar = str.charAt(0);
+  let allIndexes = [0]
+
+  for (let pos =1; pos<str.length; pos++){
+    let index = str.indexOf(firstChar,pos)
+
+    if (index>=0){
+        allIndexes.push(index)
+        pos = index+1
+    }
+  }
+  return allIndexes
+
+  //compare the following ones
+
+  //if it reaches the other indexes theres a pattern
+
+}
+
+
+console.log(findRecurrent('abcabcabc'));
 //evaluate recurring pattern
 //1. store all numbers of division until a limit
 //2. get index of all starting numbers and store in array.
